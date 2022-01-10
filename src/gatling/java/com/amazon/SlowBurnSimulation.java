7abce8106ae1e7a8279349d0a68c9e1872a51283
Steven Bayer <smbayer@amazon.com>
2022-01-10 16:47:41 -0600

    Added slow burn scenario
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

---
+++ src/gatling/java/com/amazon/SlowBurnSimulation.java
@@ -0,0 +1,30 @@
+package com.amazon;
+
+import com.amazon.tools.Chain;
+import com.amazon.tools.Protocol;
+import io.gatling.javaapi.core.CoreDsl;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import io.gatling.javaapi.core.Simulation;
+
+import java.time.Duration;
+
+public class SlowBurnSimulation extends Simulation {
+    private static final Integer largeBatchSize = 200;
+    private static final Integer rampUsers = 60;
+    private static final Duration rampUpTime = Duration.ofMinutes(60);
+    private static final Duration peakLoadTime = Duration.ofMinutes(10);
+
+    ScenarioBuilder rampUpScenario = CoreDsl.scenario("Slow Burn")
+            .during(peakLoadTime)
+            .on(Chain.sendApacheCommonLogPostRequest("Post logs with large batch", largeBatchSize));
+
+    {
+        setUp(
+                rampUpScenario.injectOpen(
+                        CoreDsl.rampUsers(rampUsers).during(rampUpTime)
+                )
+        ).protocols(
+                Protocol.httpProtocol()
+        );
+    }
+}
