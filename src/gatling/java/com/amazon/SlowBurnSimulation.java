863a8ff44eb35447e8b28b605919939b20042556
Steven Bayer <smbayer@amazon.com>
2022-01-11 14:02:17 -0600

    Update slow burn
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- src/gatling/java/com/amazon/SlowBurnSimulation.java
+++ src/gatling/java/com/amazon/SlowBurnSimulation.java
@@ -10,20 +10,22 @@ import java.time.Duration;
 
 public class SlowBurnSimulation extends Simulation {
     private static final Integer largeBatchSize = 200;
-    private static final Integer rampUsers = 60;
-    private static final Duration rampUpTime = Duration.ofMinutes(60);
+    private static final Integer rampUsers = 10;
+    private static final Duration rampUpTime = Duration.ofMinutes(1);
+    private static final Duration peakDuration = Duration.ofMinutes(1);
 
     ScenarioBuilder rampUpScenario = CoreDsl.scenario("Slow Burn")
-            .during(rampUpTime)
+            .forever()
             .on(Chain.sendApacheCommonLogPostRequest("Post logs with large batch", largeBatchSize));
 
     {
         setUp(
                 rampUpScenario.injectOpen(
-                        CoreDsl.rampUsers(rampUsers).during(rampUpTime)
+                        CoreDsl.rampUsers(rampUsers).during(rampUpTime),
+                        CoreDsl.nothingFor(peakDuration)
                 )
-        ).protocols(
-                Protocol.httpProtocol()
-        );
+        )
+                .maxDuration(rampUpTime.plus(peakDuration))
+                .protocols(Protocol.httpProtocol());
     }
 }
